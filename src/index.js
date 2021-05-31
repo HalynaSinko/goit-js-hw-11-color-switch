0c79c665ba06ea641d469c7a0fdde00a88754932
Halyna Sinko <galinasinko14@gmail.com>
2021-05-31 12:28:05 +0300

    Добавляет скрипт для реализации переключателя цвета.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,51 @@
-import { colors } from './colors.js'
+import { colors } from './colors.js';
+
+const refs = {
+  srart: document.querySelector('[data-action="start"]'),
+  stop: document.querySelector('[data-action="stop"]'),
+  body: document.body,
+};
+
+let intervalColorSwitchId = null;
+
+refs.srart.addEventListener('click', onStartClick);
+
+//При нажатии на кнопку Старт:
+//  - кнопка Старт стает не активной;
+//  - меняем бекграунд боди на рандомный в первую секунду;
+//  - запускаем интервал для рандомного изменения бекграунда боди каждую последующею секунду;
+//  - на кнопку Стоп добавляем слушателя событий;
+
+//При нажатии на Стоп :
+//  - останавливается интервал изменения бекграунда;
+//  - кнопка Старт стает активной;
+//  - с кнопки Стоп удаляем слушателя событий;
+
+function onStartClick(e) {
+  refs.srart.disabled = true;
+  
+  colorSwitch();
+ 
+  intervalColorSwitchId = setInterval(colorSwitch, 1000);
+  // console.log(colorSwitchId);
+  refs.stop.addEventListener('click', onStopClick);
+}
+
+function onStopClick() {
+  refs.srart.disabled = false;
+  // console.log('Клик на кнопку Стоп');
+ 
+  clearInterval(intervalColorSwitchId);
+
+  refs.stop.removeEventListener('click', onStopClick);
+}
+
+function colorSwitch() {
+  refs.body.style.backgroundColor =
+    colors[randomIntegerFromInterval(0, colors.length - 1)];
+};
+
+// функция для генерации случайного числа
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
